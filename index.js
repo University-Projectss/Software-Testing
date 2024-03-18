db88ec321e1630541cb13b340ff2e6a9a5dcefd4
Robert Udrea <robert.udrea@cst.ro>
2024-03-18 15:24:03 +0200

    wip prima functie din cursu 1

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const sum = (a, b) => {
-  return a + b;
+//Testare functionala, structurala si cu mutatii
+const readline = require("node:readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const getInput = async (prompt = "") => {
+  return new Promise((resolve) => {
+    readline.question(prompt, (answer) => {
+      resolve(answer);
+    });
+  });
 };
 
-export default sum;
+// #1 Implementare exemplu de testare functionala din cursul 1
+const findNumberInString = (n, bigString, charToSearch) => {
+  if (String(bigString).length !== n || typeof n !== "number") return null;
+
+  const pos = String(bigString).indexOf(charToSearch);
+
+  if (pos === -1) {
+    return "Character not found.";
+  } else {
+    return pos;
+  }
+};
+
+module.exports({
+  findNumberInString,
+});
--- index.test.js
+++ index.test.js
@@ -1,5 +1,125 @@
-import sum from ".";
+const findNumberInString = require("./index");
 
-test("Properly adds two numbers", () => {
-  expect(sum(1, 2)).toBe(3);
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const charaters = [
+  " ",
+  "!",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "[",
+  "\\",
+  "]",
+  "^",
+  "_",
+  "`",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+
+function generateRandomString(length) {
+  let randomString = "";
+  const arrayLength = charaters.length;
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * arrayLength);
+    randomString += charaters[randomIndex];
+  }
+
+  return randomString;
+}
+
+test("Find number in string", () => {
+  let n, bigString, charToSearch;
+
+  n = getRandomInt(-100000, 0);
+  bigString = generateRandomString(n);
+  expect(findNumberInString(n));
 });
